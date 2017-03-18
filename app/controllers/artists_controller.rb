8d5a7e5f6bfa467063579eb42096e09723a4be96
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 15:10:26 +0100

    Create Artists controller

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,16 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, except: [:index]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show; end
+
+  def destroy; end
+
+  private
+    def set_artist
+      @artist = Artist.find.(params[:id])
+    end
+end
