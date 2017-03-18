eba1f668eb84ad69e40e4c98c80b6815494c4b09
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 14:26:39 +0100

    Add associations and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'artists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:index, :show, :destroy]
+  resources :songs, only: [:new, :create, :destroy]
 end
