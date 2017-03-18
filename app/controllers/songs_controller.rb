eaabfa94b76e2671a998be25f09f9ffda40b8967
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 16:14:13 +0100

    Add song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,19 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
-  def create; end
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to artists_path
+    else
+      redirect_to new_song_path
+    end
+  end
 
   def destroy; end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :length, :artist_id)
+    end
 end
