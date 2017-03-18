e5c26fef93e80d5445490369f0c6521be60f36c9
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 17:40:13 +0100

    Add validation test for song

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without title" do
+      song = Song.new(title:"")
+      song.valid?
+      expect(song.errors).to have_key(:title)
+    end
+
+    it "is invalid without artist" do
+      song = Song.new(artist:nil)
+      song.valid?
+      expect(song.errors).to have_key(:artist)
+    end
+  end
 end
