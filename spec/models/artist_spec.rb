1055d47b4d6d4bf7e16ca972c790fd39557ebb45
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 17:56:52 +0100

    Add association test artist has many songs

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe Artist, type: :model do
       expect(artist.errors).to have_key(:image)
     end
   end
+
+
+  describe "association with song" do
+    let(:artist) { create :artist }
+    let!(:song) { create :song, artist: artist }
+
+    it "has many songs" do
+      song1 = artist.songs.new
+      song2 = artist.songs.new
+
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+    end
+
+    it "deletes associated songs" do
+      expect{ artist.destroy }.to change(Song, :count).by(-1)
+    end
+  end
 end
