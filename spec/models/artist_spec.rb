57cca0a1dff63a5b9beaaa7f338c6d8cf3aa7787
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 17:34:26 +0100

    Add validation test artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a name" do
+      artist = Artist.new(name: "")
+      artist.valid?
+      expect(artist.errors).to have_key(:name)
+    end
+
+    it "is invalid without an image" do
+      artist = Artist.new(image: "")
+      artist.valid?
+      expect(artist.errors).to have_key(:image)
+    end
+  end
 end
