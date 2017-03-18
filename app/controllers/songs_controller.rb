6d9c285ffc344110a9dd489b4e84e2848edb648a
anikaSchwing <anikaschwing@gmail.com>
2017-03-18 16:57:02 +0100

    Add nested routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,23 @@
 class SongsController < ApplicationController
   def new
     @song = Song.new
+    @artist = Artist.find(params[:artist_id])
   end
 
   def create
-    @song = Song.new(song_params)
+    @artist = Artist.find(params[:artist_id])
+    @song = Song.new(song_params.merge(artist: @artist))
     if @song.save
-      redirect_to artists_path
+      redirect_to artist_path(@artist.id)
     else
-      redirect_to new_song_path
+      redirect_to new_artist_song_path(:artist_id)
     end
   end
 
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to artists_path
+    redirect_to artist_path(params[:artist_id])
   end
 
   private
