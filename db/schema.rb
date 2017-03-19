f67fba8edc9a7d9f0eb011089dbf6ae26cbb88cc
anikaSchwing <anikaschwing@gmail.com>
2017-03-19 08:43:03 +0100

    Removed length from song table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318131252) do
+ActiveRecord::Schema.define(version: 20170319074032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20170318131252) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.time     "length"
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
