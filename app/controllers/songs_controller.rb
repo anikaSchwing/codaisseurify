18e39770e78a675f86e04b072b85ecd7018925a5
anikaSchwing <anikaschwing@gmail.com>
2017-03-19 09:06:18 +0100

    Refactor song model

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,14 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+
   def new
     @song = Song.new
-    @artist = Artist.find(params[:artist_id])
   end
 
   def create
-    @artist = Artist.find(params[:artist_id])
     @song = Song.new(song_params.merge(artist: @artist))
     if @song.save
-      redirect_to artist_path(@artist.id)
+      redirect_to_artist
     else
       redirect_to new_artist_song_path(@artist.id)
     end
@@ -17,11 +17,19 @@ class SongsController < ApplicationController
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to artist_path(params[:artist_id])
+    redirect_to_artist
   end
 
   private
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def redirect_to_artist
+      redirect_to artist_path(@artist.id)
+    end
+
     def song_params
-      params.require(:song).permit(:title, :length, :artist_id)
+      params.require(:song).permit(:title, :artist_id)
     end
 end
